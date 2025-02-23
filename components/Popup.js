459d5724b024b143b516f6d4bf67a57877128c1b
NOE BALADAD <NOEBALADAD@GMAIL.COM>
2025-02-23 13:03:36 -1000

    sprint project 8 updates

--- components/Popup.js
+++ components/Popup.js
@@ -1,33 +1,33 @@
 export default class Popup {
-  constructor(popupSelector) {
-    this._popupElement = document.querySelector(popupSelector);
-    this._handleEscapeClose = this._handleEscapeClose.bind(this);
-  }
-
-  open() {
-    this._popupElement.classList.add("popup_visible");
-    document.addEventListener("keydown", this._handleEscapeClose);
-  }
-
-  close() {
-    this._popupElement.classList.remove("popup_visible");
-    document.removeEventListener("keydown", this._handleEscapeClose);
-  }
-
-  _handleEscapeClose(event) {
-    if (event.key === "Escape") {
-      this.close();
+    constructor(popupSelector) {
+      this._popupElement = document.querySelector(popupSelector);
+      this._handleEscapeClose = this._handleEscapeClose.bind(this);
     }
-  }
-
-  setEventListeners() {
-    this._popupElement.addEventListener("mousedown", (event) => {
-      if (
-        event.target.classList.contains("popup_visible") ||
-        event.target.classList.contains("popup__close")
-      ) {
+  
+    open() {
+      this._popupElement.classList.add("popup_visible");
+      document.addEventListener("keydown", this._handleEscapeClose);
+    }
+  
+    close() {
+      this._popupElement.classList.remove("popup_visible");
+      document.removeEventListener("keydown", this._handleEscapeClose);
+    }
+  
+    _handleEscapeClose(event) {
+      if (event.key === "Escape") {
         this.close();
       }
-    });
+    }
+  
+    setEventListeners() {
+      this._popupElement.addEventListener("mousedown", (event) => {
+        if (
+          event.target.classList.contains("popup_visible") ||
+          event.target.classList.contains("popup__close")
+        ) {
+          this.close();
+        }
+      });
+    }
   }
-}
